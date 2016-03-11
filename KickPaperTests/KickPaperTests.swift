f306e2fb4261c97e6517330b7c293253a1bc3d9e
Jouderian Nobre <jouderianjr@gmail.com>
2016-03-11 01:25:33 -0300

    Initial Commit

---
+++ KickPaperTests/KickPaperTests.swift
@@ -0,0 +1,36 @@
+//
+//  KickPaperTests.swift
+//  KickPaperTests
+//
+//  Created by Jouderian Ferreira Nobre Junior on 3/11/16.
+//  Copyright © 2016 Jouderian Ferreira Nobre Junior. All rights reserved.
+//
+
+import XCTest
+@testable import KickPaper
+
+class KickPaperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
